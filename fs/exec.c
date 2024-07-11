3decf1bf07fe8c4b9d1ad7e79db27ceda1f0f6f1
Lunarixus <Nathan@lunarixus.dev>
2024-07-11 04:13:42 +0100

    fs: Drop redundant KSU hooks when CONFIG_KPROBES is enabled
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

--- fs/exec.c
+++ fs/exec.c
@@ -1719,7 +1719,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 #ifdef CONFIG_KSU
-extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
@@ -1739,10 +1738,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 	int retval;
 
 #ifdef CONFIG_KSU
-	if (unlikely(ksu_execveat_hook))
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-	else
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 #endif
 
 	if (IS_ERR(filename))
